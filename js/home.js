ae9c67338041ada274cdb645645654a28aba6f40
maxmutov-nuriddin <mahmudovnuriddin35@gmail.com>
2023-08-10 08:35:26 +0500

    Revert "Revert "add""
    This reverts commit dda05dd54be888b74cc4e3916afd6bafa91e3a48.

--- js/home.js
+++ js/home.js
@@ -65,6 +65,10 @@ specialOffer.map((cards) => {
 
 
 function getProductCard(product) {
+  let check = cart.find((pr) => pr.id === product.id);
+
+
+
   const productCard = document.createElement("div");
   productCard.className = "promotion__box";
 
@@ -108,10 +112,12 @@ function getProductCard(product) {
   productRatings.className = 'promotion__rating'
 
 
-  const productBtn = document.createElement("a");
-  productBtn.href = '../basket.html'
+  const productBtn = document.createElement("button");
   productBtn.className = 'promotion__btn'
   productBtn.innerHTML = "В корзину";
+
+  productBtn.addEventListener("click", () => addToCart(product.id));
+
 
   productCardFooter.prepend(productBtn);
   productCardFooter.prepend(productRatings);
@@ -122,6 +128,28 @@ function getProductCard(product) {
 
   return productCard;
 }
+
+function addToCart(id) {
+  let product = products.find((pr) => pr.id === id);
+  let check = cart.find((pr) => pr.id === id);
+
+  if (check) {
+    cart = cart.map((pr) => {
+      if (pr.id === id) {
+        pr.quantity++;
+      }
+      return pr;
+    });
+  } else {
+    product.quantity = 1;
+    cart.push(product);
+  }
+  localStorage.setItem("cart", JSON.stringify(cart));
+  getProducts();
+  getCartTotal();
+}
+
+
 
 products.slice(1, 5).map((product) => {
   let card = getProductCard(product);
